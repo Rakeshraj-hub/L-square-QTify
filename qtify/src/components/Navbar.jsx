ef3302aa054c5bacbc0fc4357cdfca3041174cc8
Rakesh Raj Mullick <jimmymorrios@gmail.com>
2025-05-05 16:33:54 +0530

    test i

--- qtify/src/components/Navbar.jsx
+++ qtify/src/components/Navbar.jsx
@@ -1,31 +1,29 @@
-// Navbar.jsx
 import React from 'react';
 import './Navbar.css';
-import { FaSearch } from 'react-icons/fa';  // react-icons package
-import qicon from '../assets/Group1.png'; 
+import { FaSearch } from 'react-icons/fa';
+import qicon from '../assets/Group1.png';
 
 function Navbar() {
   return (
     <div className="navbar">
-      {/* Left - Logo */}
+      {/* Logo */}
       <div className="navbar-left">
-        <img src={qicon} className="logo-icon" />
-        {/* <span className="brand-name">tify</span> */}
+        <img src={qicon} alt="App Logo" className="logo-icon" />
       </div>
 
-      {/* Center - Search */}
+      {/* Search Bar */}
       <div className="navbar-center">
         <input
           type="text"
           placeholder="Search a album of your choice"
           className="search-input"
         />
-        <button className="search-button">
+        <button className="search-button" aria-label="search">
           <FaSearch />
         </button>
       </div>
 
-      {/* Right - Button */}
+      {/* Feedback Button */}
       <div className="navbar-right">
         <button className="feedback-button">Give Feedback</button>
       </div>
